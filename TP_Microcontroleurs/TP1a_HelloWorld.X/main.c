c04c86e325f5937af764896908e0775dc7b12a46
LOZANO-A <anthony.lozano@student.junia.com>
2025-01-24 10:29:51 +0100

    TP helloWorls + init chenillard

--- TP_Microcontroleurs/TP1a_HelloWorld.X/main.c
+++ TP_Microcontroleurs/TP1a_HelloWorld.X/main.c
@@ -1,59 +1,36 @@
 #include "configbits.h" // Bits de configuration
 #include <xc.h>         // Definition des registres specifiques au uC
 
-// #definitions
-#define MASK_LED1 0x01 // Définit un masque permettant d'isoler la LED D1 (bit #0 du port D) -> 0000.0001
-#define MASK_LED2 0x02
-#define MASK_LED3 0x04
-#define MASK_LED4 0x08
-#define MASK_LED5 0x01 //// Définit un masque permettant d'isoler la LED D5 (bit #0 du port B)
-#define MASK_LED6 0b00000010
-#define MASK_LED7 0b00000100
-#define MASK_LED8 0b00001000
+
+// Configuration des ports pour les LEDs
+#define LEDS_D1_D4 LATB
+#define LEDS_D5_D8 LATC
+
+// Prototypes des fonctions
+void delai_approx(void);
 
 void main(void) {
-    /* Code d'initialisation */
+    // Configuration des ports en sortie
+    TRISD = 0x00; // Port B en sortie led D1 a D4
+    TRISB = 0x00; // Port D en sortie led D5 a D8
 
-    TRISD &= ~MASK_LED1; // Data direction - Déterminer si c'est une entrée ou une sortie où D nom du port - Sortie 0 
-    TRISD &= ~MASK_LED2;
-    TRISD &= ~MASK_LED3;
-    TRISD &= ~MASK_LED4;
-    TRISB &= ~MASK_LED5;
-    TRISB &= ~MASK_LED6;
-    TRISB &= ~MASK_LED7;
-    TRISB &= ~MASK_LED8;
-    
-   while(1){
-        // Code Ã  exÃ©cuter Ã  l'infini
-        LATD |= MASK_LED1; // Output latch - Permet de lire, écrire et modifier la valeur du MASC -> allumer la LED1
-        __delay_ms(250); // Attend pendant 250 ms
-        LATD = 0x00; // Eteindre toutes les LEDs de D
-        LATD |= MASK_LED2;
-        __delay_ms(250);
-        LATD = 0x00; 
-        LATD |= MASK_LED3;
-        __delay_ms(250);
-        LATD = 0x00;
-        LATD |= MASK_LED4;
-        __delay_ms(250);
-        LATB = 0x00; // Eteindre toutes les LEDs de B
-        LATD = 0x00; // continuer d'éteidre toutes les LEDs de D car sinon LED4 reste allumée
-        LATB |= MASK_LED5;
-        __delay_ms(250);
-        LATB = 0x00;
-        LATD = 0x00;
-        LATB |= MASK_LED6;
-        __delay_ms(250);
-        LATB = 0x00;
-        LATD = 0x00;
-        LATB |= MASK_LED7;
-        __delay_ms(250);
-        LATB = 0x00;
-        LATD = 0x00;
-        LATB |= MASK_LED8;
-        __delay_ms(250);
-        LATB = 0x00;
-        LATD = 0x00;
+    while (1) {
+        // Allumer les LEDs D1-D4
+        LATD = 0x0F; // 00001111 en binaire
+        LATB = 0x00; // 00000000 en binaire
+        delai_approx(); //attend
+
+        // Allumer les LEDs D5-D8
+        LATD = 0x00; // 00000000 en binaire
+        LATB = 0x0F; // 00001111 en binaire
+        delai_approx(); //attend
     }
-   
+}
+
+void delai_approx(void) {
+    // Temporisation approximative d'environ 1 seconde
+    for (unsigned long i = 0; i < 90000; i++) 
+    {
+        //boucle vide pour attendre 1 seconde
+    }
 }
