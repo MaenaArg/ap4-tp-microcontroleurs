de3375a2f975784720b090f2a1ef5db1ef3d9aad
MaenaArg <argano.maena@gmail.com>
2025-02-10 15:36:43 +0100

    TP2a_pwm changement intensité via bouton FONCTIONNEL

--- TP_Microcontroleurs/TP2a_pwm.X/main.c
+++ TP_Microcontroleurs/TP2a_pwm.X/main.c
@@ -1,69 +1,35 @@
 #include "configbits.h"
 #include <xc.h>
-//PWM
 
-
-// Définitions des masques LED
+// Définitions des masques 
 #define MASK_LED1 0x01
-#define MASK_LED2 0x02
-#define MASK_LED3 0x04
-#define MASK_LED4 0x08
-#define MASK_LED5 0x01
-#define MASK_LED6 0x02
-#define MASK_LED7 0x04
-#define MASK_LED8 0x08
-
-int led_index = 0;
-int timer_count = 0;
-int cpt = 0;
 
 void main(void) {
     /* Configuration des ports */
-    TRISD &= ~(MASK_LED1 | MASK_LED2 | MASK_LED3 | MASK_LED4);
-    TRISB &= ~(MASK_LED5 | MASK_LED6 | MASK_LED7 | MASK_LED8);
-    
-    // Eteindre toutes les LEDs
-    LATD = 0x00;
-    LATB = 0x00;
-
-    // Configuration de INTCON pour l'interruption
-    INTCONbits.GIE = 1;
-    INTCONbits.PEIE = 1;
-    //PIE1 = 0b00000010;
-    PIE1bits.TMR2IE=1;
-    
+    TRISD &= ~(MASK_LED1);
+    TRISBbits.TRISB0 = 1;  // Configuration du bouton S1 en entrée
+    ANSELBbits.ANSB0 = 0;  // On ne veut pas d'entrée analogique
+            
+    /* Configuration matérielle PWM via PPS */
+    RD0PPS = 0b00001111;  // Car d'après la doc si on a RD0PPS = 01111 alors on se connecte à la sortie PWM4OUT
+      
+    PWM4CONbits.PWM4EN = 1;
+    PWM4DCH = 0x0C;   // Car 0x0C = 12 => 10% de 124 (PR2)
+    PWM4DCL = 0;
+       
+    /* Configuration Timer2 */
     PR2 = 124;
-    //T2CON = 0b01111100; // Prescaler 1, Postscaler 1:16, Timer2 activé
-    T2CONbits.TMR2ON=1; // the timer is enable
-    T2CONbits.T2CKPS=0b00; // Prescaler ? 1:1
-    T2CONbits.T2OUTPS=0b1111; // Postscaler ? 1:16
-    
-    while(1){ }
-}
+    T2CONbits.TMR2ON = 1; // the timer is enable
+    T2CONbits.T2CKPS = 0b00; // Prescaler ? 1:1
+    T2CONbits.T2OUTPS = 0b1111; // Postscaler ? 1:16
 
-void __interrupt() isr (void) {
-    //interruption toutes les 125ml secondes          
-    PIR1bits.TMR2IF = 0;  // Effacez le drapeau d'interruption
-    if(cpt==125){
-        // Eteindre toutes les LEDs
-        LATD = 0x00;
-        LATB = 0x00;
-        // Allume LED une après l'autre
-        switch (led_index) {
-            //fait un OU entre valeur de la sortie (LATD) et le masque de la LED
-            case 0: LATD |= MASK_LED1; break;
-            case 1: LATD |= MASK_LED2; break;
-            case 2: LATD |= MASK_LED3; break;
-            case 3: LATD |= MASK_LED4; break;
-            case 4: LATB |= MASK_LED5; break;
-            case 5: LATB |= MASK_LED6; break;
-            case 6: LATB |= MASK_LED7; break;
-            case 7: LATB |= MASK_LED8; break;
-        }
-        cpt = 0;
-        led_index++;
-        if (led_index >= 8) led_index = 0;
-    } else {
-        cpt++;
-    }              
+    while(1){
+        PWM4DCH = 0x0C;   // Car 0x0C = 12 => 10% de 124 (PR2)
+        PWM4DCL = 0;
+        
+        if(PORTBbits.RB0 == 0){
+           PWM4DCH = 0x7C; // 100%
+           PWM4DCL = 0;
+        } 
+    }
 }
